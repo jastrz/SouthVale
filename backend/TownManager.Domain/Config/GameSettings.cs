723d6cf004c68c7057020ae52b613ed7e94d7edc
jastrz <jastrzebski.j.m@gmail.com>
2026-07-24 16:35:11 +0200

    Fixed healthcheck db log spam; Increased production to 16x

--- backend/TownManager.Domain/Config/GameSettings.cs
+++ backend/TownManager.Domain/Config/GameSettings.cs
@@ -8,6 +8,6 @@ public static class GameSettings
     public const int MaxVillagesPerPlayer = 8;
     public static Resources StartingResources => new(800, 800, 800, 800);
     public static float TravelSpeedMultiplier { get; set; } = 10f;
-    public static float ResourcesProductionMultiplier { get; set; } = 4f;
+    public static float ResourcesProductionMultiplier { get; set; } = 16f;
     public static int ConfigVersion { get; set; }
 }
